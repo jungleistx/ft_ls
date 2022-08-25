511906b3d77b0c5fd948343cc8d38c06712fde03
Roope Vuorenlehto <rvuorenl@c3r2p6.hive.fi>
2022-08-25 14:11:50 +0300

    add list_sort.c

---
+++ list_sort.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_sort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 13:50:21 by rvuorenl          #+#    #+#             */
+/*   Updated: 2022/08/22 12:46:16 by rvuorenl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void	update_list_time(t_node *prev, t_node *node, t_node **head, int *sort)
+{
+	t_node	*tmp;
+
+	(*sort)++;
+	if (prev)
+		prev->next = node->next;
+	else
+		*head = node->next;
+	prev = node->next;
+	tmp = node->next->next;
+	node->next->next = node;
+	node->next = tmp;
+}
+
+//	first node is smallest number (newest)
+void	list_sort_time_reverse(t_node **head)
+{
+	t_node	*tmp;
+	t_node	*prev;
+	int		sorted;
+
+	sorted = 0;
+	while (!sorted)
+	{
+		tmp = *head;
+		while (tmp && tmp->next)
+		{
+			if (tmp->sec < tmp->next->sec && tmp->n_sec < tmp->next->n_sec &&
+			tmp->name < tmp->next->name)
+				update_list_time(prev, tmp, head, &sorted);
+			else
+			{
+				prev = tmp;
+				tmp = tmp->next;
+			}
+		}
+		if (sorted == 0)
+			return ;
+		sorted = 0;
+	}
+}
+
+// first node is biggest number (oldest)
+void	list_sort_time(t_node **head)
+{
+	t_node	*tmp;
+	t_node	*prev;
+	int		sorted;
+
+	sorted = 0;
+	while (!sorted)
+	{
+		tmp = *head;
+		while (tmp && tmp->next)
+		{
+			if (tmp->sec > tmp->next->sec && tmp->n_sec > tmp->next->n_sec &&
+			tmp->name > tmp->next->name)
+				update_list_time(prev, tmp, head, &sorted);
+			else
+			{
+				prev = tmp;
+				tmp = tmp->next;
+			}
+		}
+		if (sorted == 0)
+			return ;
+		sorted = 0;
+	}
+}

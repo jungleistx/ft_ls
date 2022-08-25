0d642a74cca09c408b3d97b0ed0b77f328a01e37
Roope Vuorenlehto <rvuorenl@c3r2p6.hive.fi>
2022-08-25 14:10:26 +0300

    add read_arguments to utilities.c

--- utilities.c
+++ utilities.c
@@ -6,18 +6,19 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 15:03:32 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/08/17 15:06:12 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/08/25 13:50:14 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	reset_info(t_info *info, t_node *head)
+void	reset_info(t_info *info, t_node **head)
 {
 	info->options = 0;
 	info->args = 0;
-	info->path = NULL; 		// needed ?
-	head = NULL;
+	*head = NULL;
+	info->ret_nr = 0;
+	// info->path = NULL; 		// needed ?
 }
 
 int	option_validity(char *str)
@@ -77,4 +78,27 @@ void	read_options(int argc, char **argv, t_info *info)
 	}
 	if (i < argc)
 		info->args = argc - i;
+}
+
+void	read_arguments(t_node **head, int argc, char **argv, t_info *info)
+{
+	struct stat	filestat;
+
+	if (info->args == 0)
+	{
+		// printf("to list_add\n");
+		list_add_directory(head, ".", info->options);
+
+	}
+	else
+	{
+		// printf("to list_add_else\n");
+		while (info->args > 0)
+		{
+			lstat(argv[argc - info->args], &filestat);
+			// printf("read = '%s'\n", filestat.st_nam)
+			create_node(head, filestat, argv[argc - info->args], info->options);
+			info->args--;
+		}
+	}
 }

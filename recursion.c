1f756bd934b687edb0ffb4cc46dd52c668cf8bff
Roope Vuorenlehto <rvuorenl@c3r3p2.hive.fi>
2022-08-26 15:15:27 +0300

    add recursion.c, print_find_dir

---
+++ recursion.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   recursion.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 12:20:35 by rvuorenl          #+#    #+#             */
+/*   Updated: 2022/08/26 14:56:30 by rvuorenl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+t_node	*print_list_find_dir(t_node *head, int opts)
+{
+	t_node	*tmp;
+	t_node	*dir;
+
+	tmp = head;
+	dir = NULL;
+	while (tmp)
+	{
+		if (opts & LONG)
+			print_long_list_node(tmp);
+		else
+			printf("%s\n", tmp->name);
+		if (tmp->type == 4 && !dir)		//  find the first dir
+			dir = tmp;
+		tmp = tmp->next;
+	}
+	return (dir);
+}

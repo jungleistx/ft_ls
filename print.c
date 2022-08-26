efcd6b8a2e5e18596c308043f834d19a16d73d7c
Roope Vuorenlehto <rvuorenl@c3r3p2.hive.fi>
2022-08-26 15:00:20 +0300

    add print_dir_recursive to print.c

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 15:04:00 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/08/25 21:03:47 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/08/26 14:58:27 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,19 @@ void	print_list_errors(t_node *head, int *ret_nr)
 
 void	print_long_list_node(t_node *node)
 {
-	printf("%s %*d %s  %s %*d %s %s\n",node->l_opt->permissions, 3,
-		node->l_opt->links, node->l_opt->owner, node->l_opt->group, 8,
-		node->l_opt->size, node->l_opt->date, node->name);// 8?
+	// printf("%s %*d %s  %s %*d %s %s\n",node->l_opt->permissions, 3,
+	// 	node->l_opt->links, node->l_opt->owner, node->l_opt->group, 8,
+	// 	node->l_opt->size, node->l_opt->date, node->name);// 8?
+	printf("%s   ", node->l_opt->permissions);
+	printf("%d   ", node->l_opt->links);
+	printf("%s   ", node->l_opt->owner);
+	printf("%s   ", node->l_opt->group);
+	printf("%d   ", node->l_opt->size);
+	printf("%s   ", node->l_opt->date);
+	printf("%s \n", node->name);
+	// printf("%s %d %s %s %d %s %s\n", node->l_opt->permissions,
+	// 	node->l_opt->links, node->l_opt->owner, node->l_opt->group,
+	// 	node->l_opt->size, node->l_opt->date, node->name);
 }
 
 void	print_long_list(t_node *head)
@@ -42,9 +52,19 @@ void	print_long_list(t_node *head)
 	tmp = head;
 	while (tmp)
 	{
-		printf("%s %*d %s  %s %*d %s %s\n",tmp->l_opt->permissions, 3,
-			tmp->l_opt->links, tmp->l_opt->owner, tmp->l_opt->group, 8,
-			tmp->l_opt->size, tmp->l_opt->date, tmp->name);// 8?
+		printf("%s   ", tmp->l_opt->permissions);
+		printf("%d   ", tmp->l_opt->links);
+		printf("%s   ", tmp->l_opt->owner);
+		printf("%s   ", tmp->l_opt->group);
+		printf("%d   ", tmp->l_opt->size);
+		printf("%s   ", tmp->l_opt->date);
+		printf("%s \n", tmp->name);
+		// printf("%s %d %s %s %d %s %s\n", tmp->l_opt->permissions,
+		// 	tmp->l_opt->links, tmp->l_opt->owner, tmp->l_opt->group,
+		// 	tmp->l_opt->size, tmp->l_opt->date, tmp->name);
+		// printf("%s %*d %s  %s %*d %s %s\n",tmp->l_opt->permissions, 3,
+		// 	tmp->l_opt->links, tmp->l_opt->owner, tmp->l_opt->group, 8,
+		// 	tmp->l_opt->size, tmp->l_opt->date, tmp->name);// 8?
 		tmp = tmp->next;
 	}
 }
@@ -59,7 +79,7 @@ void	print_list_files(t_node **head, int options)
 		if (tmp->type != 4 && tmp->type != 0)
 		{
 			if (options & LONG)
-				print_long_list(tmp);
+				print_long_list_node(tmp);
 			else
 				printf("%s\n", tmp->name);
  		}
@@ -72,17 +92,17 @@ void	print_list_files(t_node **head, int options)
 	// print_test(*head);
 }
 
-void	print_list_all(t_node *head, int opts)
+void	print_list_all(t_node *head)
 {
 	t_node	*tmp;
 
 	tmp = head;
 	while (tmp)
 	{
-		printf("%s\n", tmp->name);
+		printf("%s\t", tmp->name);
 		tmp = tmp->next;
 	}
-	free_file_nodes(&head, opts);
+	printf("\n");
 }
 
 void	print_list(t_node *head, int opts)
@@ -90,7 +110,7 @@ void	print_list(t_node *head, int opts)
 	if (opts & LONG)
 		print_long_list(head);
 	else
-		print_list_all(head, opts);
+		print_list_all(head);
 }
 
 void	print_dir(t_node *head, int opts)
@@ -98,35 +118,41 @@ void	print_dir(t_node *head, int opts)
 	t_node	*tmp;
 	t_node	*newhead;
 
+	newhead = NULL;
 	tmp = head;
 	while (tmp)
 	{
-		printf("%s:\n", tmp->name);
+		printf("\n%s:\n", tmp->name);
 		list_add_directory(&newhead, tmp->name, opts);
+		// tmp->name works as a path, UNTESTED
 		print_list(newhead, opts);
 		tmp = tmp->next;
 		free_list(&newhead, opts);
 	}
-	// free_list(head, opts);
 }
 
-void	print_dir_recursive(t_node *head, int opts)
+void	print_dir_recursive(t_node **head, int opts)
 {
 	t_node	*tmp;
 	t_node	*newhead;
+	t_node	*rec_head;
 
+	rec_head = NULL;
+	newhead = NULL;
 	tmp = head;
 	while (tmp)
 	{
-		printf("%s:\n", tmp->name);
+		printf("\n%s:\n", tmp->name);
 		list_add_directory(&newhead, tmp->name, opts);
-		print_list(newhead, opts);
+		rec_head = print_list_find_dir(newhead, opts);
+		if (rec_head)
+			print_dir_recursive(&rec_head, opts);
 		tmp = tmp->next;
+		free_list(&newhead, opts);
 	}
 }
 
 /*
-
 recursion:
 
 make list of node->name
@@ -134,5 +160,4 @@ print list
 free filenodes (in print)
 if folders -> recursion tmp->name
 free list
-
 */

1aacd6d61dffa2fc24b4f70d59f8b5e32c3317e8
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-09-19 15:38:57 +0300

    fix norm recursion.c

--- recursion.c
+++ recursion.c
@@ -6,28 +6,48 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 12:20:35 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/09/13 15:25:49 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/09/19 15:21:40 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-t_node	*print_list_find_dir(t_node *head, int opts)
+void	check_null_head(t_node **head, t_info *info)
+{
+	if (*head != NULL)
+	{
+		print_list(head, info);
+		free_file_nodes(head, info->options);
+	}
+	else
+		printf("\n");
+}
+
+void	print_dir_recursive(t_node **head, t_info *info)
 {
 	t_node	*tmp;
-	t_node	*dir;
+	t_node	*newhead;
 
-	tmp = head;
-	dir = NULL;
+	newhead = NULL;
+	tmp = *head;
 	while (tmp)
 	{
-		if (opts & LONG)
-			print_long_list_node(tmp);
-		else
-			printf("%s\n", tmp->name);
-		if (tmp->type == 4 && !dir)		//  find the first dir
-			dir = tmp;
+		if (tmp->name[0] != '.')
+		{
+			if (info->options & DIR_NAME)
+				print_path(tmp->path, info->options);
+			list_add_directory(&newhead, tmp->path, info, tmp->name);
+			if (info->options & LONG && newhead != NULL)
+				printf("total %ld\n", info->total);
+			check_null_head(&newhead, info);
+			if (newhead)
+			{
+				info->options |= DIR_NAME;
+				print_dir_recursive(&newhead, info);
+			}
+			free_list(&newhead, info->options);
+		}
 		tmp = tmp->next;
 	}
-	return (dir);
+	free_list(head, info->options);
 }

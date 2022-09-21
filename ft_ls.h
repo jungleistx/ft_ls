e48eff85a8e90d3c45abd2f30372433e5481c0e2
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-09-21 21:43:14 +0300

    change duplicate names in printf.h and ft_ls.h

--- ft_ls.h
+++ ft_ls.h
@@ -6,25 +6,26 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 19:01:47 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/09/20 14:09:56 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/09/21 20:23:42 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-# include <stdio.h>
-# include <string.h>
 # include <time.h>
 # include <dirent.h>
 # include <sys/stat.h>
 # include <pwd.h>
 # include <sys/types.h>
 # include <grp.h>
-# include <unistd.h>
-// # include "./libft/libft.h"
-# include <stdlib.h>
 # include <sys/ioctl.h>
+# include <stdio.h>
+# include "printf/includes/ft_printf.h"
+// # include "libft/libft.h"
+// # include <string.h>
+// # include <unistd.h>
+// # include <stdlib.h>
 
 # define OPTIONS "lRart"
 
@@ -51,17 +52,17 @@ typedef struct s_node
 	struct s_long	*l_opt;
 }					t_node;
 
-typedef struct s_info
+typedef struct s_inf
 {
 	int			ret_nr;
 	uint16_t	options;
 	int			args;
 	long		total;
-}				t_info;
+}				t_inf;
 
 typedef enum e_options
 {
-	LONG = 1 << 0,
+	LON = 1 << 0,
 	RECURSIVE = 1 << 1,
 	HIDDEN = 1 << 2,
 	REVERSE = 1 << 3,
@@ -92,32 +93,32 @@ void	list_sort_add(t_node **head, t_node *node, int options);
 int		check_link_validity(t_node *node, int opts);
 int		node_filetype(struct stat filestat, t_node *node, int opts);
 char	*get_full_path(char *name, char *path);
-void	create_node(t_node **head, char *name, t_info *info, char *path);
+void	create_node(t_node **head, char *name, t_inf *info, char *path);
 
 // print.c
-void	print_long_list(t_node *head, t_info *info);
-void	print_list_all(t_node *head, t_info *info);
-void	print_list(t_node **head, t_info *info);
-void	print_dir(t_node *head, t_info *info);
-void	print_path(char *str, t_info *info);
+void	print_long_list(t_node *head, t_inf *info);
+void	print_list_all(t_node *head, t_inf *info);
+void	print_list(t_node **head, t_inf *info);
+void	print_dir(t_node *head, t_inf *info);
+void	print_path(char *str, t_inf *info);
 
 // print2.c
-void	print_list_errors(t_node *head, t_info *info);
+void	print_list_errors(t_node *head, t_inf *info);
 void	print_long_list_node(t_node *node);
-void	print_list_files(t_node **head, t_info *info);
-void	print_free_list(t_node **head, t_info *info);
+void	print_list_files(t_node **head, t_inf *info);
+void	print_free_list(t_node **head, t_inf *info);
 
 // utilities.c
 int		option_validity(char *str);
-void	print_current_dir(t_node **head, t_info *info);
-void	read_arguments(t_node **head, int argc, char **argv, t_info *info);
-void	read_options(int argc, char **argv, t_info *info);
-void	shift_options(char *argv, t_info *info);
+void	print_current_dir(t_node **head, t_inf *info);
+void	read_arguments(t_node **head, int argc, char **argv, t_inf *info);
+void	read_options(int argc, char **argv, t_inf *info);
+void	shift_options(char *argv, t_inf *info);
 
 // recursion.c
-void	check_null_head(t_node **head, t_info *info);
+void	check_null_head(t_node **head, t_inf *info);
 int		traverse_hidden_files(char *name);
-void	print_dir_recursive(t_node **head, t_info *info);
+void	print_dir_recursive(t_node **head, t_inf *info, int dot_dir);
 
 // list_sort.c
 void	update_list_time(t_node **prev, t_node *node, t_node **head, int *sort);
@@ -131,28 +132,29 @@ void	list_add_long_filetype(t_node *node, struct stat filestat, int a);
 void	exit_readlink_error(char *str);
 void	print_sym_link(t_node *node, int opts);
 void	add_symbolic_link(t_node *node);
-void	list_add_long(t_node *node, struct stat filestat, t_info *info);
+void	list_add_long(t_node *node, struct stat filestat, t_inf *info);
 
 // ft_ls.c
 char	*ft_strdup_exit(char *src);
-void	reset_info(t_info *info, t_node **head);
-void	list_add_directory(t_node **head, char *path, t_info *info, char *name);
-void	ft_ls(t_node **head, t_info *info);
+void	reset_info_ls(t_inf *info, t_node **head);
+void	list_add_directory(t_node **head, char *path, t_inf *info, char *name);
+void	ft_ls(t_node **head, t_inf *info);
 // int	main
 
 // libft
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	ft_memdel(void **ap);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_putstr_fd(char const *s, int fd);
-char	*ft_strcat(char *s1, const char *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-int		ft_strcmp(const char *s1, const char *s2);
-char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strncpy(char *dst, const char *src, size_t len);
-size_t	ft_strlen(const char *s);
+// int		ft_atoi(const char *str);
+// void	ft_bzero(void *s, size_t n);
+// void	ft_memdel(void **ap);
+// void	*ft_memset(void *b, int c, size_t len);
+// int		ft_printf(const char *str, ...);
+// void	ft_putstr_fd(char const *s, int fd);
+// char	*ft_strcat(char *s1, const char *s2);
+// char	*ft_strchr(const char *s, int c);
+// char	*ft_strdup(const char *s1);
+// int		ft_strcmp(const char *s1, const char *s2);
+// char	*ft_strcpy(char *dst, const char *src);
+// char	*ft_strncpy(char *dst, const char *src, size_t len);
+// size_t	ft_strlen(const char *s);
 
 // void	print_list_node(t_node *node, int opts);
 // void	list_sort_time_dispatch(t_node **head, int opts);

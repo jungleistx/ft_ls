a8eca8d310d2e5203cc5ae73ad71c1b182df73a9
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-09-22 14:44:20 +0300

    add utilities2.c with print_year, get_owner_group and free_error_nodes

---
+++ srcs/utilities2.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utilities2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 14:42:04 by rvuorenl          #+#    #+#             */
+/*   Updated: 2022/09/22 14:43:24 by rvuorenl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_ls.h"
+
+void	free_error_nodes(t_node **head, int options)
+{
+	t_node	*tmp;
+	t_node	*prev;
+	t_node	*next;
+
+	tmp = *head;
+	prev = NULL;
+	while (tmp)
+	{
+		if (tmp->type == 0)
+		{
+			next = tmp->next;
+			if (!prev)
+				*head = next;
+			free_node(tmp, options);
+			tmp = next;
+			if (prev)
+				prev->next = tmp;
+		}
+		else
+		{
+			prev = tmp;
+			tmp = tmp->next;
+		}
+	}
+}
+
+void	get_owner_group(t_node *node, struct stat filestat)
+{
+	struct passwd	*user;
+	struct group	*g_id;
+
+	user = getpwuid(filestat.st_uid);
+	if (!user)
+		node->l_opt->owner = ft_itoa((int)filestat.st_uid);
+	else
+		node->l_opt->owner = ft_strdup_exit(user->pw_name);
+	g_id = getgrgid(filestat.st_gid);
+	if (!g_id)
+		node->l_opt->group = ft_itoa((int)filestat.st_gid);
+	else
+		node->l_opt->group = ft_strdup_exit(g_id->gr_name);
+}
+
+void	print_year(t_node *node)
+{
+	time_t		cur_time;
+	struct stat	filestat;
+
+	if (lstat(node->path, &filestat) == -1)
+	{
+		perror(node->name);
+		exit(6);
+	}
+	if (time(&cur_time) == -1)
+	{
+		perror("time");
+		exit(5);
+	}
+	if (cur_time - filestat.st_ctimespec.tv_sec > (2629743 * 6)
+		|| filestat.st_ctimespec.tv_sec - cur_time > (2629743 * 6))
+	{
+		write (1, node->l_opt->date, 7);
+		ft_printf ("%d  ", node->l_opt->year);
+	}
+	else
+		ft_printf("%s ", node->l_opt->date);
+}

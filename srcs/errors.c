e30a832e637de5a48597e5ee699efdb614d813e4
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-09-23 13:26:30 +0300

    fix errors to redirect to fd2

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,7 +6,7 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 15:00:31 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/09/20 15:45:49 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/09/23 13:25:49 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 void	error_notfound(char *filename)
 {
-	ft_printf("ft_ls: %s: No such file or directory\n", filename);
+	ft_putstr_fd("ft_ls: ", 2);
+	ft_putstr_fd(filename, 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
 }
 
 void	exit_illegal(void)
 {
-	ft_printf("ft_ls: illegal option -- -\n");
-	ft_printf("usage: ./ft_ls [-alrRt] [file ...]\n");
+	ft_putstr_fd("ft_ls: illegal option -- -\n", 2);
+	ft_putstr_fd("usage: ./ft_ls [-alrRt] [file ...]\n", 2);
 	exit(1);
 }
 
